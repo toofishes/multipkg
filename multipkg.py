a7e48611d6415021b38832c32b3b4af84a5caedb
Dan McGee <dan@archlinux.org>
2009-10-31 10:45:13 -0500

    Big changes
    Hack the script a lot more to make it usable.
    
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- multipkg.py
+++ multipkg.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 
+
+# TODO list:
+# * Support multiple cache directories (but keep sane default)
+# * Support binding to only certain interfaces
+# * XXX Exclude databases from being found elsewhere
+# * XXX Support full file paths
+# * Have group awareness so we can timeout before 0.500 seconds
+#   if everyone has responded
+# * XXX Ignore multicast messages from self
+
 from __future__ import with_statement
 
-import os, socket, threading
+import os, re, socket, threading
 
 from twisted.internet.protocol import DatagramProtocol
-from twisted.internet import reactor, defer
+from twisted.internet import reactor, defer, threads
 
 from twisted.application.internet import MulticastServer
 
@@ -16,33 +26,46 @@ from twisted.web.util import Redirect
 from twisted.web.error import NoResource
 
 DEF_ADDR = '224.0.0.156'
-DEF_PORT = 36992
+DEF_PORT = 8954
+MCAST = (DEF_ADDR, DEF_PORT)
 
-class PackageCache():
-    cache = dict()
+class PackageRequest():
+    def __init__(self, queue, package):
+        print "new request: %s" % package
+        self.event = threading.Event()
+        self.queue = queue
+        self.pkgname = package
+        self.address = None
+
+    def wait(self):
+        self.event.wait(0.500)
+        queue.remove(self)
+        return self.address
+
+
+class PackageRequestQueue():
+    requests = []
     lock = threading.Lock()
 
-    def clear(self):
+    def new(self, package):
         with self.lock:
-            self.cache.clear()
+            ret = PackageRequest(self, package)
+            self.requests.append(ret)
+            return ret
 
-    def add(self, package, address):
+    def found(self, package, address):
         with self.lock:
-            if package not in self.cache:
-                self.cache[package] = []
-            addrs = self.cache.get(package)
-            if address not in addrs:
-                self.cache.get(package).append(address)
-
-    def get(self, package):
-        ret = None
+            for r in self.requests:
+                if r.pkgname == package:
+                    r.address = address
+                    r.event.set()
+    
+    def remove(self, item):
         with self.lock:
-            if package in self.cache:
-                ret = self.cache.get(package)[0]
-        return ret
+            self.requests.remove(item)
 
     def debug(self):
-        print repr(self.cache)
+        print repr(self.requests)
 
 def find_package(pkgfile):
     # TODO multiple cache directories
@@ -52,82 +75,88 @@ def find_package(pkgfile):
     return None
 
 def find_remote_package(pkgfile):
-    global mcastclient
-    print "looking for %s" % pkgfile
-    mcastclient.write(pkgfile, (DEF_ADDR, DEF_PORT))
+    global mcastserver
+    print "multicast search: %s" % pkgfile
+    mcastserver.write("search:%s" % pkgfile, MCAST)
 
-class MulticastPackageServer(DatagramProtocol):
+class MulticastPackageFinder(DatagramProtocol):
     def startProtocol(self):
-        print 'Started Listening'
+        self.transport.setLoopbackMode(False)
         self.transport.joinGroup(DEF_ADDR)
 
-    def stopProtocol(self):
-        print 'Stopped Listening'
+    message_re = re.compile('(.*?):(.*)')
+    def parse_message(self, message):
+        m = self.message_re.match(message)
+        if m:
+            return m.groups()[0], m.groups()[1]
+        else: 
+            return None, None
 
     def datagramReceived(self, datagram, address):
-        print "Server Received:" + repr(datagram) + repr(address)
-        path = find_package(datagram)
-        if path != None:
-            self.transport.write("yes:%s" % datagram, address)
-        else:
-            self.transport.write("no:%s" % datagram, address)
-
-
-class MulticastPackageClient(DatagramProtocol):
-    def datagramReceived(self, datagram, address):
-        print "Client Received:" + repr(datagram) + repr(address)
-        if datagram.startswith("yes:"):
-            global cache
-            cache.add(datagram[4:], address[0])
-        cache.debug()
+        global queue
+        print "received: %s %s" % (address[0], datagram)
+        type, contents = self.parse_message(datagram)
+        if type == "search":
+            path = find_package(contents)
+            if path != None:
+                self.transport.write("yes:%s" % contents, MCAST)
+            else:
+                self.transport.write("no:%s" % contents, MCAST)
+        elif type == "yes":
+            queue.found(contents, address[0])
 
 
 class MulticastPackageResource(Resource):
-    def _delayedRender(self, request):
-        global cache
-        cache.debug()
-        address = cache.get(request.prepath[-1])
+    def deferred_search(self, request):
+        global queue
+        pkgname = request.prepath[-1]
+        qi = queue.new(pkgname)
+        find_remote_package(pkgname)
+        address = qi.wait()
         if address != None:
-            print "package found at %s" % address
+            print "found: %s %s" % (address, pkgname)
             resource = Redirect("http://%s:%s/cache/%s" % (address, "8080", request.prepath[-1]))
             resource.render(request)
         else:
-            print "returning not found"
             resource = NoResource()
             resource.render(request)
         request.finish()
         return request
 
     def render_GET(self, request):
-        print repr(request)
-        find_remote_package(request.prepath[-1])
-        d = defer.Deferred()
-        d.addCallback(self._delayedRender)
-        reactor.callLater(0.400, d.callback, request)
+        d = threads.deferToThread(self.deferred_search, request)
         return NOT_DONE_YET
 
 
 # package serving and searching setup
 class SearchResource(Resource):
+    def is_database(self, path):
+        return path.endswith(".db.tar.gz")
+
     def getChild(self, path, request):
-        print "req: %s" % path
+        # if we aren't at the last level, try to get there
+        if len(request.postpath) > 0:
+            return SearchResource()
+        print "search request: %s" % path
+        if self.is_database(path):
+            return NoResource()
         # the local case is the easiest- check our own cache first
         localpath = find_package(path)
         if localpath != None:
-            #return Redirect("/cache/%s" % path)
-			return File(localpath)
+            return File(localpath)
         # ok, time to multicast search
         return MulticastPackageResource()
 
-cache = PackageCache()
+queue = PackageRequestQueue()
 
 httproot = Resource()
 httproot.putChild("cache", File("/var/cache/pacman/pkg"))
 httproot.putChild("search", SearchResource())
 
 # get our various listeners, clients, etc. set up
-mcastserver = reactor.listenMulticast(DEF_PORT, MulticastPackageServer())
-mcastclient = reactor.listenUDP(0, MulticastPackageClient())
+mcastserver = reactor.listenMulticast(DEF_PORT, MulticastPackageFinder())
 reactor.listenTCP(8080, Site(httproot))
 
 reactor.run()
+
+# vim: set et:
