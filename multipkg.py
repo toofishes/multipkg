6c64b660ae51845c35f0e13852ab130a3be09538
Dan McGee <dan@archlinux.org>
2009-10-31 13:24:40 -0500

    More changes
    Getting awesomer all the time
    
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- multipkg.py
+++ multipkg.py
@@ -1,23 +1,53 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2009 Dan McGee.
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 # TODO list:
-# * Support multiple cache directories (but keep sane default)
+# * XXX Support multiple cache directories (but keep sane default)
 # * Support binding to only certain interfaces
-# * XXX Exclude databases from being found elsewhere
-# * XXX Support full file paths
 # * Have group awareness so we can timeout before 0.500 seconds
 #   if everyone has responded
-# * XXX Ignore multicast messages from self
+# * Add a default mirror capability
 
 from __future__ import with_statement
 
 import os, re, socket, threading
+import sys
+
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
 
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor, defer, threads
 
 from twisted.application.internet import MulticastServer
+
+from twisted.python import log
 
 from twisted.web.resource import Resource
 from twisted.web.server import Site, NOT_DONE_YET
@@ -28,6 +58,9 @@ from twisted.web.error import NoResource
 DEF_ADDR = '224.0.0.156'
 DEF_PORT = 8954
 MCAST = (DEF_ADDR, DEF_PORT)
+
+cachedirs = [ "/var/cache/pacman/pkg",
+              "/var/cache/makepkg/pkg" ]
 
 class PackageRequest():
     def __init__(self, queue, package):
@@ -59,6 +92,10 @@ class PackageRequestQueue():
                 if r.pkgname == package:
                     r.address = address
                     r.event.set()
+
+    def notfound(self, package, address):
+        # TODO
+        pass
     
     def remove(self, item):
         with self.lock:
@@ -68,21 +105,33 @@ class PackageRequestQueue():
         print repr(self.requests)
 
 def find_package(pkgfile):
-    # TODO multiple cache directories
-    path = "/var/cache/pacman/pkg/%s" % pkgfile
-    if os.path.isfile(path) and os.access(path, os.R_OK):
-        return path
+    for dir in cachedirs:
+        if os.path.isdir(dir):
+            path = os.path.join(dir, pkgfile)
+            if os.path.isfile(path) and os.access(path, os.R_OK):
+                return path
     return None
 
 def find_remote_package(pkgfile):
-    global mcastserver
     print "multicast search: %s" % pkgfile
     mcastserver.write("search:%s" % pkgfile, MCAST)
 
 class MulticastPackageFinder(DatagramProtocol):
+    def __init__(self, queue):
+        self.queue = queue
+
+    def leave(self):
+        self.send("gone:")
+
     def startProtocol(self):
+        # join our group, don't listen to ourself, and announce our presence
         self.transport.setLoopbackMode(False)
         self.transport.joinGroup(DEF_ADDR)
+        self.send("ping:")
+        reactor.addSystemEventTrigger("before", "shutdown", self.leave)
+
+    def send(self, message):
+        self.transport.write(message, MCAST)
 
     message_re = re.compile('(.*?):(.*)')
     def parse_message(self, message):
@@ -93,31 +142,53 @@ class MulticastPackageFinder(DatagramProtocol):
             return None, None
 
     def datagramReceived(self, datagram, address):
-        global queue
+        '''
+        The important stuff happens here. When we get a multicast message in,
+        we want to process it and take action as necessary. Currently the
+        following message types are known:
+        * search - sender is looking for a file with the given name
+        * found - sender has the file with the given name
+        * notfound - sender does NOT have the file with the given name
+        * ping - sender is looking for other clustermates
+        * pong - sender is announcing presence in cluster
+        * gone - sender is about to shutdown and leave cluster
+        '''
         print "received: %s %s" % (address[0], datagram)
         type, contents = self.parse_message(datagram)
         if type == "search":
             path = find_package(contents)
             if path != None:
-                self.transport.write("yes:%s" % contents, MCAST)
+                self.send("found:%s" % contents)
             else:
-                self.transport.write("no:%s" % contents, MCAST)
-        elif type == "yes":
-            queue.found(contents, address[0])
+                self.send("notfound:%s" % contents)
+        elif type == "found":
+            self.queue.found(contents, address[0])
+        elif type == "notfound":
+            self.queue.notfound(contents, address[0])
+        elif type == "ping":
+            self.send("pong:")
+        elif type == "pong":
+            pass
+        elif type == "gone":
+            pass
 
 
 class MulticastPackageResource(Resource):
+    def __init__(self, queue):
+        self.queue = queue
+        Resource.__init__(self)
+
     def deferred_search(self, request):
-        global queue
         pkgname = request.prepath[-1]
-        qi = queue.new(pkgname)
+        qi = self.queue.new(pkgname)
         find_remote_package(pkgname)
         address = qi.wait()
         if address != None:
             print "found: %s %s" % (address, pkgname)
-            resource = Redirect("http://%s:%s/cache/%s" % (address, "8080", request.prepath[-1]))
+            resource = Redirect("http://%s:%s/cache/%s" % (address, str(DEF_PORT), pkgname))
             resource.render(request)
         else:
+            print "not found: %s" % pkgname
             resource = NoResource()
             resource.render(request)
         request.finish()
@@ -130,32 +201,58 @@ class MulticastPackageResource(Resource):
 
 # package serving and searching setup
 class SearchResource(Resource):
-    def is_database(self, path):
-        return path.endswith(".db.tar.gz")
+    def __init__(self, queue):
+        self.queue = queue
+        Resource.__init__(self)
+
+    def is_allowed(self, path):
+        '''
+        There are certain things we don't want to ever share. These include
+        databases and old-style package names without an architecture. You may
+        ask "why not use different ports per architecture?" If we did this,
+        you'd get no benefit when it comes to arch=any packages, which is the
+        whole idea of them.
+        '''
+        if path.endswith(".db.tar.gz"):
+            return False
+        oldskool = re.compile('.*-[0-9\.]+\.pkg\.tar\.gz$')
+        if oldskool.match(path):
+            return False
+        return True
 
     def getChild(self, path, request):
         # if we aren't at the last level, try to get there
         if len(request.postpath) > 0:
-            return SearchResource()
+            return SearchResource(self.queue)
         print "search request: %s" % path
-        if self.is_database(path):
+        if not self.is_allowed(path):
             return NoResource()
         # the local case is the easiest- check our own cache first
         localpath = find_package(path)
         if localpath != None:
             return File(localpath)
         # ok, time to multicast search
-        return MulticastPackageResource()
+        return MulticastPackageResource(self.queue)
+
+
+class CacheResource(Resource):
+    def getChild(self, path, request):
+        localpath = find_package(path)
+        if localpath != None:
+            return File(localpath)
+        return NoResource()
+ 
+log.startLogging(sys.stdout)
 
 queue = PackageRequestQueue()
 
 httproot = Resource()
-httproot.putChild("cache", File("/var/cache/pacman/pkg"))
-httproot.putChild("search", SearchResource())
+httproot.putChild("search", SearchResource(queue))
+httproot.putChild("cache", CacheResource())
 
 # get our various listeners, clients, etc. set up
-mcastserver = reactor.listenMulticast(DEF_PORT, MulticastPackageFinder())
-reactor.listenTCP(8080, Site(httproot))
+mcastserver = reactor.listenMulticast(DEF_PORT, MulticastPackageFinder(queue))
+httpserver = reactor.listenTCP(DEF_PORT, Site(httproot))
 
 reactor.run()
 
